9dbcf3edf59cfabbd24cc419cbabfffddca96c20
georgeroubie <george.roubie@gmail.com>
2022-03-07 00:20:51 +0200

    Update eslint rules

--- src/app/routing/Routing.jsx
+++ src/app/routing/Routing.jsx
@@ -4,14 +4,12 @@ import AddPlayer from '../pages/AddPlayer';
 import Play from '../pages/Play';
 import Players from '../pages/Players';
 
-const Routing = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Play />} />
-      <Route path="/players" element={<Players />} />
-      <Route path="/add-player" element={<AddPlayer />} />
-    </Routes>
-  );
-};
+const Routing = () => (
+  <Routes>
+    <Route path="/" element={<Play />} />
+    <Route path="/players" element={<Players />} />
+    <Route path="/add-player" element={<AddPlayer />} />
+  </Routes>
+);
 
 export default Routing;
