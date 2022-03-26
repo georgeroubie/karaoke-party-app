c2d3ad36f1e5c076dca7502d4cb4408b03442bbd
georgeroubie <george.roubie@gmail.com>
2022-03-26 15:13:54 +0200

    Remove roboto font and use Arial

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 const typography = css`
   body {
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Arial', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
