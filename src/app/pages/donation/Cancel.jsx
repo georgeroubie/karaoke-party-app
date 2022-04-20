521df21e7febc2a86045563ae2a21a674c5f0581
georgeroubie <george.roubie@gmail.com>
2022-04-20 16:46:22 +0300

    Update cancel donation message

--- src/app/pages/donation/Cancel.jsx
+++ src/app/pages/donation/Cancel.jsx
@@ -2,8 +2,8 @@ import PageWrapper from '../../components/PageWrapper';
 
 const CancelDonation = () => (
   <PageWrapper title="No worries">
-    You changed your mind for the donation and that's a good thing. <br /> Those who can’t change their minds can’t
-    change anything.
+    You changed your mind for the donation and that's fine. <br /> Those who can’t change their minds can’t change
+    anything.
   </PageWrapper>
 );
 
