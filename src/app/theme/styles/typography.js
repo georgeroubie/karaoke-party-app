cb066c77e3e12add5ad9ad4e658982718f7dc301
georgeroubie <george.roubie@gmail.com>
2022-08-27 12:30:14 +0300

    Respect user settings for font size

--- src/app/theme/styles/typography.js
+++ src/app/theme/styles/typography.js
@@ -7,7 +7,7 @@ const typography = css`
     -moz-osx-font-smoothing: grayscale;
     background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
     color: ${({ theme: { colors } }) => colors.textPrimary};
-    font-size: 16px;
+    font-size: 1rem;
     font-weight: ${({ theme: { fontWeight } }) => fontWeight.normal};
     line-height: 1.4rem;
     letter-spacing: 0.05rem;
