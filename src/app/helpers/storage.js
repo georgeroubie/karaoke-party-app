1a80cfc4031f1727a3058eaf3417d5b2fd555c3d
George Roubie <george.roubie@gmail.com>
2022-10-17 09:47:30 +0300

    Update storage.js

--- src/app/helpers/storage.js
+++ src/app/helpers/storage.js
@@ -1,11 +1,15 @@
 const SAVED_THEME_KEY = 'user_selected_theme';
 const LOCAL_STORAGE_KEY = 'players_list';
 
-const getSavedTheme = () => localStorage.getItem(SAVED_THEME_KEY);
+function getSavedTheme() {
+  localStorage.getItem(SAVED_THEME_KEY);
+}
 
-const saveThemeSelection = (theme) => localStorage.setItem(SAVED_THEME_KEY, theme);
+function saveThemeSelection(theme) {
+  localStorage.setItem(SAVED_THEME_KEY, theme);
+}
 
-const getSavedPlayersList = () => {
+function getSavedPlayersList() {
   const list = localStorage.getItem(LOCAL_STORAGE_KEY);
   if (list) {
     try {
@@ -16,16 +20,16 @@ const getSavedPlayersList = () => {
     }
   }
   return [];
-};
+}
 
-const savePlayersList = (list) => {
+function savePlayersList(list) {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(list));
-};
+}
 
-const deleteAllData = () => {
+function deleteAllData() {
   localStorage.removeItem(SAVED_THEME_KEY);
   localStorage.removeItem(LOCAL_STORAGE_KEY);
   window.location.href = '/';
-};
+}
 
 export { getSavedTheme, saveThemeSelection, getSavedPlayersList, savePlayersList, deleteAllData };
