cf9a12fb5ecdef72167dc5d9299e63756745b69e
George Roubie <george.roubie@gmail.com>
2022-10-17 09:47:16 +0300

    Update Complete.jsx

--- src/app/pages/donation/Complete.jsx
+++ src/app/pages/donation/Complete.jsx
@@ -1,10 +1,12 @@
 import PageWrapper from '../../components/layout/PageWrapper';
 
-const CompleteDonation = () => (
-  <PageWrapper title="You are awesome">
-    Your donation is very prized and will forever be held in high regard in my memory. For this, I thank you and greatly
-    appreciate your support. Also I respect and admire your decision to donate so selflessly.
-  </PageWrapper>
-);
+const CompleteDonation = () => {
+  return (
+    <PageWrapper title="You are awesome">
+      Your donation is very prized and will forever be held in high regard in my memory. For this, I thank you and
+      greatly appreciate your support. Also I respect and admire your decision to donate so selflessly.
+    </PageWrapper>
+  );
+};
 
 export default CompleteDonation;
