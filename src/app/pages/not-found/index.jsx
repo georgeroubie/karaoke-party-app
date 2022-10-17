4f28dd2f32d1d5a5d40d8e3d0b8e89ee5e76383d
George Roubie <george.roubie@gmail.com>
2022-10-17 09:46:50 +0300

    Update index.jsx

--- src/app/pages/not-found/index.jsx
+++ src/app/pages/not-found/index.jsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom';
 import PageWrapper from '../../components/layout/PageWrapper';
 import Description from '../../components/typography/Description';
 
-const NotFound = () => (
-  <PageWrapper title="Oops :(">
-    <Description>The page you are looking for does not exist!</Description>
-    <Description>
-      Click <Link to="/">here</Link> to go to home page.
-    </Description>
-  </PageWrapper>
-);
+const NotFound = () => {
+  return (
+    <PageWrapper title="Oops :(">
+      <Description>The page you are looking for does not exist!</Description>
+      <Description>
+        Click <Link to="/">here</Link> to go to home page.
+      </Description>
+    </PageWrapper>
+  );
+};
 
 export default NotFound;
