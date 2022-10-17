6d13bc757214e12514acbb588bf45dbc5234b6e2
George Roubie <george.roubie@gmail.com>
2022-10-17 09:46:56 +0300

    Update Wrapper.jsx

--- src/app/pages/sing/Wrapper.jsx
+++ src/app/pages/sing/Wrapper.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 import PageWrapper from '../../components/layout/PageWrapper';
 
-const Wrapper = ({ children }) => <PageWrapper title="Party time 🎉">{children}</PageWrapper>;
+const Wrapper = ({ children }) => {
+  return <PageWrapper title="Party time 🎉">{children}</PageWrapper>;
+};
 
 Wrapper.propTypes = {
   children: PropTypes.node.isRequired,
