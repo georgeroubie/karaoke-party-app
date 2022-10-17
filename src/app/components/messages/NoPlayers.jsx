de0756d38ea024532f4c83f38bdb7b1d1ca25321
George Roubie <george.roubie@gmail.com>
2022-10-17 09:47:42 +0300

    Update NoPlayers.jsx

--- src/app/components/messages/NoPlayers.jsx
+++ src/app/components/messages/NoPlayers.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import Warning from '../typography/Warning';
 
-const NoPlayers = () => (
-  <Warning>
-    No players, don't worry <Link to="/add-player">add some</Link>
-  </Warning>
-);
+const NoPlayers = () => {
+  return (
+    <Warning>
+      No players, don't worry <Link to="/add-player">add some</Link>
+    </Warning>
+  );
+};
 
 export default NoPlayers;
