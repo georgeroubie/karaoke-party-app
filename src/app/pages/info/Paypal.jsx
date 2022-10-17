d8a1e16f05b6b68c75ff15a9a7493fa6b5e36c48
George Roubie <george.roubie@gmail.com>
2022-10-17 09:47:14 +0300

    Update Paypal.jsx

--- src/app/pages/info/Paypal.jsx
+++ src/app/pages/info/Paypal.jsx
@@ -9,19 +9,21 @@ const Input = styled.input``;
 
 const Image = styled.img``;
 
-const Paypal = () => (
-  <Wrapper action="https://www.paypal.com/donate" method="post" target="_top">
-    <Input type="hidden" name="hosted_button_id" value="FA7AF4NJTZVEG" />
-    <Input
-      type="image"
-      src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"
-      border="0"
-      name="submit"
-      title="PayPal - The safer, easier way to pay online!"
-      alt="Donate with PayPal button"
-    />
-    <Image alt="" border="0" src="https://www.paypal.com/en_GR/i/scr/pixel.gif" width="1" height="1" />
-  </Wrapper>
-);
+const Paypal = () => {
+  return (
+    <Wrapper action="https://www.paypal.com/donate" method="post" target="_top">
+      <Input type="hidden" name="hosted_button_id" value="FA7AF4NJTZVEG" />
+      <Input
+        type="image"
+        src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"
+        border="0"
+        name="submit"
+        title="PayPal - The safer, easier way to pay online!"
+        alt="Donate with PayPal button"
+      />
+      <Image alt="" border="0" src="https://www.paypal.com/en_GR/i/scr/pixel.gif" width="1" height="1" />
+    </Wrapper>
+  );
+};
 
 export default Paypal;
